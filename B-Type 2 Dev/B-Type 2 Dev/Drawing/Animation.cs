5f0ba7e5d25095b10c64ba227199b9c03ebbeb4d
Fernando Chorney <fernando@rolloutstudios.com>
2011-09-27 13:08:08 -0500

    Added Animation and Entity class. Will be used for the player class later

---
+++ B-Type 2 Dev/B-Type 2 Dev/Drawing/Animation.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace B_Type_2_Dev.Drawing
+{
+    public class Frame
+    {
+        private String _AssetName;
+        private Texture2D _Texture;
+        private Rectangle _SourceRectangle;
+
+        public virtual int Height
+        {
+            get { return _Texture.Height; }
+        }
+
+        public virtual int Width
+        {
+            get { return _Texture.Width; }
+        }
+
+        public virtual Texture2D Texture
+        {
+            get { return _Texture; }
+        }
+
+        public virtual Rectangle SourceRectangle
+        {
+            get { return _SourceRectangle; }
+        }
+
+        internal Frame() { }
+
+        public Frame(string assetName, Rectangle sourceRectangle)
+        {
+            _AssetName = assetName;
+            _SourceRectangle = sourceRectangle;
+            LoadResources();
+        }
+
+        public void LoadResources()
+        {
+            // Load Texture
+            //_Texture = G.Content.Load<Texture2D>(_AssetName);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+    }
+
+    public class Animation : Frame
+    {
+        public List<Frame> Frames;
+        public int CurrentFrame;
+        public double AnimationSpeed;
+        private double _TimeInFrame;
+
+        internal int _TotalFrames;
+
+        public int TotalFrames
+        {
+            get { return _TotalFrames; }
+        }
+
+        public override Texture2D Texture
+        {
+            get { return Frames[CurrentFrame].Texture; }
+        }
+
+        public override Rectangle SourceRectangle
+        {
+            get { return Frames[CurrentFrame].SourceRectangle; }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _TimeInFrame += gameTime.ElapsedGameTime.TotalSeconds;
+            if (AnimationSpeed > 0 && _TimeInFrame >= AnimationSpeed)
+            {
+                CurrentFrame++;
+                if (CurrentFrame == TotalFrames)
+                    CurrentFrame = 0;
+                _TimeInFrame = 0;
+            }
+        }
+    }
+
+    public class AnimationFactory
+    {
+        public static Animation GenerateAnimation(string assetName, int frameWidth, int frameHeight, int animLength, double animSpeed)
+        {
+            Animation _Animation = new Animation();
+            Texture2D _Texture = null; //G.Content.Load<Texture2D>(assetName);
+
+            int Cols = _Texture.Width / frameWidth;
+            int rows = _Texture.Height / frameHeight;
+
+            _Animation.Frames = new List<Frame>();
+            _Animation.CurrentFrame = 0;
+            _Animation._TotalFrames = animLength; //Cols * Rows
+
+            for (int j = 0; j < rows; j++)
+                for (int i = 0; i < Cols && j * Cols + i < animLength; i++)
+                {
+                    Frame _Frame = new Frame(assetName, new Rectangle(i * frameWidth, j * frameHeight, frameWidth, frameHeight));
+                    _Animation.Frames.Add(_Frame);
+                }
+
+            _Animation.AnimationSpeed = animSpeed;
+            return _Animation;
+        }
+    }
+}
---
+++ B-Type 2 Dev/B-Type 2 Dev/Drawing/Entity.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace B_Type_2_Dev.Drawing
+{
+    public interface ITransformable
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        float Scale { get; set; }
+        float Rotation { get; set; }
+    }
+
+    class Entity : ITransformable
+    {
+        private Frame _Frame;
+        private Color _Color;
+        private Vector2 _position;
+        private float _scale;
+        private float _rotation;
+
+        public Frame Frame
+        {
+            get { return _Frame; }
+        }
+
+        public Animation Animation
+        {
+            get { return _Frame as Animation; }
+        }
+
+        public int Height
+        {
+            get { return _Frame.SourceRectangle.Height; }
+        }
+
+        public int Width
+        {
+            get { return _Frame.SourceRectangle.Width; }
+        }
+
+        public float X
+        {
+            get { return _position.X; }
+            set { _position.X = value; }
+        }
+
+        public float Y
+        {
+            get { return _position.Y; }
+            set { _position.Y = value; }
+        }
+
+        public float Scale
+        {
+            get { return _scale; }
+            set { _scale = value; }
+        }
+
+        public float Rotation
+        {
+            get { return _rotation; }
+            set { _rotation = value; }
+        }
+
+        public Color Color
+        {
+            get { return _Color; }
+            set { _Color = value; }
+        }
+
+        public Entity(Frame frame, Vector2 startPosition)
+        {
+            _Frame = frame;
+            _position = startPosition;
+            _Color = Color.White;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _Frame.Update(gameTime);
+        }
+
+        public void Draw()
+        {
+            //G.SpriteBatch.Draw(_Frame.Texture, _position, _Frame.SourceRectangle, _Color);
+        }
+
+
+        public void Draw(Color color)
+        {
+            //G.SpriteBatch.Draw(_Frame.Texture, _position, _Frame.SourceRectangle, color);
+        }
+
+        public void Draw(float x, float y, float scale, float rotation, Color color)
+        {
+            Vector2 texCenter = new Vector2(Width / 2, Height / 2);
+            texCenter = new Vector2(0, 0);
+
+            //G.SpriteBatch.Draw(_Frame.Texture, new Vector2(x + texCenter.X, y + texCenter.Y), _Frame.SourceRectangle, color, rotation, texCenter, scale, SpriteEffects.None, 0);
+        }
+    }
+}

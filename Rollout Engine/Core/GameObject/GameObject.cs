fb0cc3afce373a535c43b7530ea451bf5c1b8fd0
Lawrence Luk <lawrence@rolloutstudios.com>
2011-10-11 20:58:06 -0500

    GameObject

---
+++ Rollout Engine/Core/GameObject/GameObject.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Rollout.Screens;
+
+namespace Rollout.Core
+{
+
+    public interface IGameObject
+    {
+        void Initialize();
+        void Update(GameTime gameTime);
+        void Draw(GameTime gameTime);
+    }
+
+    public class GameObject : IGameObject
+    {
+        public bool Enabled { get; set; }
+
+        private Screen screen;
+        public Screen Screen { get { return screen; }
+            set
+            {
+                if (screen == null)
+                {
+                    screen = value;
+                }
+            }
+        }
+
+        protected IGameObject Parent { get; set; }
+        protected List<IGameObject> Children { get; set; }
+
+        public GameObject()
+        {
+            Children = new List<IGameObject>();
+        }
+
+
+        public void Add(GameObject obj)
+        {
+            obj.Parent = this;
+            obj.Screen = screen;
+            Children.Add(obj);
+        }
+
+        public void Remove(GameObject obj)
+        {
+            if (Children.Contains(obj))
+                Children.Remove(obj);
+        }
+
+        public virtual void Initialize()
+        {
+            foreach (var gameObject in Children)
+            {
+                gameObject.Initialize();
+            }
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            foreach (var gameObject in Children)
+            {
+                gameObject.Update(gameTime);
+            }
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+            foreach (var gameObject in Children)
+            {
+                gameObject.Draw(gameTime);
+            }
+        }
+
+    }
+}

eebe28f2212567b9208144934123eb79679134e6
Fernando Chorney <fernando@rolloutstudios.com>
2011-10-18 12:29:05 -0500

    SpriteComponent looks to be functional. Cant thing of anything else to do to it at the moment. Going to merge with master.

--- B-Type 2 Dev/B-Type 2 Dev/Tests/PlayerTest.cs
+++ B-Type 2 Dev/B-Type 2 Dev/Tests/PlayerTest.cs
@@ -16,9 +16,12 @@ namespace B_Type_2_Dev
     public class PlayerTest : DrawableGameComponent
     {
         private PlayerInput input;
-        //private Sprite player;
 
         private SpriteComponent player;
+        private bool fireRight = true;
+        private Limiter fireLimit;
+
+        private double elapsedTime;
 
         public PlayerTest()
             : base(G.Game)
@@ -27,21 +30,12 @@ namespace B_Type_2_Dev
 
         public override void Initialize()
         {
-            //player = new Sprite(new Vector2(600, 320f));
-            //player.AddAnimation("main", new Animation(@"Sprites/spaceship", 64, 64, 2, new double[] { 0.5f, 1.0f }, true, 5));
-           // player.AddAnimation("alternate", new Animation(@"Sprites/spaceship2", 64, 64, 2, new double[] { 0.1f, 0.2f }));
-            //player.Animation.Loop = false;
-
-            //player.AddChild(new SubSprite(new Vector2(2,2), "main", new Animation(@"Sprites/spaceship-shadow", 64, 64, 1, new double[] {1.0f})));
-            //player.AddChild(new SubSprite(new Vector2(-21, 20), "main", new Animation(@"Sprites/gun1", 32, 32, 2, new double[] { .05f, .08f }), true));
-            //player.AddChild(new SubSprite(new Vector2(57, 20), "main", new Animation(@"Sprites/gun1", 32, 32, 2, new double[] { .08f, .05f }), true));
-
             player = new SpriteComponent();
             player.AddSprite("main", new Sprite(new Vector2(200, 200), new Animation(@"Sprites/spaceship2", 64 ,64, 2, new double[] {0.1f, 0.2f})), 4, true);
-            player.AddSprite("LeftGun", new Sprite(new Vector2(-21, 20), new Animation(@"Sprites/gun1", 32, 32, 2, new double[] {0.05f, 0.08f})), 5);
-            player.AddSprite("RightGun", new Sprite(new Vector2(57, 20), new Animation(@"Sprites/gun1", 32, 32, 2, new double[] { 0.05f, 0.08f })), 5);
+            player.AddSprite("LeftGun", new Sprite(new Vector2(-21, 20), new Animation(@"Sprites/gun1", 32, 32, 2, new double[] {0.05f, 0.08f}, false)), 5);
+            player.AddSprite("RightGun", new Sprite(new Vector2(57, 20), new Animation(@"Sprites/gun1", 32, 32, 2, new double[] { 0.05f, 0.08f }, false)), 5);
             player.AddSprite("Shadow", new Sprite(new Vector2(2,2), new Animation(@"Sprites/spaceship-shadow", 64, 64)), 3);
-            
+
             input = new PlayerInput(PlayerIndex.One);
             G.Game.Components.Add(input);
 
@@ -56,6 +50,9 @@ namespace B_Type_2_Dev
             input.BindAction("Right",Keys.Right);
             input.BindAction("Up",Keys.Up);
             input.BindAction("Down",Keys.Down);
+            input.BindAction("Fire",Keys.Space);
+
+            fireLimit = new Limiter(.05f);
 
             base.Initialize();
         }
@@ -64,27 +61,12 @@ namespace B_Type_2_Dev
         {
 
             player.Update(gameTime);
+            fireLimit.Update(gameTime);
             //player.Rotation += (float)(20f * gameTime.ElapsedGameTime.TotalSeconds);
             //player.Scale += (float)(.3f * gameTime.ElapsedGameTime.TotalSeconds);
 
-            //if (input.IsPressed("Pause"))
-            //    player.Pause();
-
             if (input.IsPressed("Quit"))
                 G.Game.Exit();
-
-            //if (input.IsPressed("UnPause"))
-            //    player.UnPause();
-
-            //if (input.IsPressed("Switch-A"))
-            //    player.SetAnimation("main");
-
-            //if (input.IsPressed("Switch-S"))
-            //    player.SetAnimation("alternate");
-
-            //if (input.IsPressed("Restart"))
-            //    player.ReStart();
-
 
             if (input.IsHeld("Left"))
                 player.X -= 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -94,6 +76,43 @@ namespace B_Type_2_Dev
                 player.Y -= 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (input.IsHeld("Down"))
                 player.Y += 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (input.IsPressed("Fire"))
+            {
+                elapsedTime = gameTime.ElapsedGameTime.TotalSeconds;
+                FireGun();
+            }
+
+            if (input.IsReleased("Fire"))
+            {
+                elapsedTime = 0;
+            }
+
+            if (input.IsHeld("Fire"))
+            {
+                elapsedTime += gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (elapsedTime > 0.2f)
+                {
+                    FireGun();
+                }
+            }
+        }
+
+        private void FireGun()
+        {
+            if (fireLimit.Ready)
+            {
+                if (fireRight)
+                {
+                    player.Sprites["RightGun"].ReStart();
+                }
+                else
+                {
+                    player.Sprites["LeftGun"].ReStart();
+                }
+                fireRight = !fireRight;
+            }
         }
 
         public override void Draw(GameTime gameTime)
